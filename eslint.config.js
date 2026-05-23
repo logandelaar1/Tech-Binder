d659b4a5b27136ed03626ac6a4321eb37c442e42
logandelaar1 <delaarlogan@gmail.com>
2026-05-22 20:13:22 -0400

    pleaseee

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig([
     files: ['src/components/ui/**/*.{ts,tsx}'],
     rules: {
       'react-refresh/only-export-components': 'off',
+      "@typescript-eslint/no-unused-vars": "warn"
     },
   },
 ])
